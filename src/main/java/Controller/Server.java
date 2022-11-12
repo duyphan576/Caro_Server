1cd266af9ba083fba17465baed2fed9d13f4f381
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2022-11-12 22:55:10 +0700

    update

--- src/main/java/Controller/Server.java
+++ src/main/java/Controller/Server.java
@@ -8,8 +8,9 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Vector;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -18,23 +19,32 @@ import java.util.concurrent.Executors;
 public class Server {
 
     public static int port = 1234;
-    public static ServerSocket server;
+    public static Socket socket;
     public static int roomId;
+    public static volatile ServerThreadBus serverThreadBus;
     public static Vector<ServerThread> clientList = new Vector<>();
-    
 
     public static void main(String[] args) throws IOException {
-        ExecutorService executor = Executors.newCachedThreadPool();
         try {
             ServerSocket server = new ServerSocket(port);
+            serverThreadBus = new ServerThreadBus();
             System.out.println("Server binding at port " + port);
             System.out.println("Waiting for client...");
-            int i =1;
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10, // corePoolSize
+                100, // maximumPoolSize
+                10, // thread timeout
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(8) // queueCapacity
+        );
+            int i = 1;
             roomId = 1000;
-            while(true) {
-                Socket socket = server.accept();
+            while (true) {
+                socket = server.accept();
                 ServerThread client = new ServerThread(socket, Integer.toString(i++));
                 clientList.add(client);
+//                serverThreadBus.add(client);
+//                System.out.println("Number of threads running is: "+serverThreadBus.getLength());
                 executor.execute(client);
             }
         } catch (IOException e) {
--- src/main/java/Controller/ServerThread.java
+++ src/main/java/Controller/ServerThread.java
@@ -15,7 +15,6 @@ import java.net.Socket;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -76,6 +75,12 @@ public class ServerThread implements Runnable {
         this.room = room;
     }
 
+    public ServerCryptography getSc() {
+        return sc;
+    }
+    
+    
+
     @Override
     public void run() {
         try {
@@ -94,7 +99,6 @@ public class ServerThread implements Runnable {
                 in.readFully(encryptedInput, 0, encryptedInput.length);
             }
             encryptedMsg = sc.processInitialMsg(encryptedInput);
-            System.out.println(encryptedMsg);
             while (true) {
                 // Servers nhận dữ liệu từ client qua stream
                 // read length of incoming message
@@ -108,34 +112,30 @@ public class ServerThread implements Runnable {
                 //Read from client: byte[] encryptedMsg
                 encryptedMsg = sc.symmetricDecryption(encryptedInput);
                 String[] part = encryptedMsg.split(";");
-                if (encryptedMsg.equals("bye")) {
+                if (encryptedMsg.equals("Exit")) {
+
                     break;
                 } else if (part[0].equals("Register")) {
                     register(part);
                 } else if (part[0].equals("Rank")) {
                     rank(part);
                 } else if (part[0].equals("Login")) {
+                    System.out.println(encryptedMsg);
                     login(part);
-                } else if (part[0].equals("FriendOnl")) {
-                    List<User> l = userDAL.findUserOnline();
-                    String tempp = String.valueOf(l.size());
-                    for (User use : l) {
-                        tempp += ";" + use.getNickname() + ";" + String.valueOf(use.getStatus());
-                    }
-                    byte[] encryptedOutput = sc.symmetricEncryption(tempp);
-                    push(encryptedOutput);
-                } else if(part[0].equals("createRoom")){
+                } else if (part[0].equals("userStatus")) {
+                    userStatus(part);
+                } else if (part[0].equals("createRoom")) {
                     createRoom(part);
+                } else if (part[0].equals("Logout")) {
+
+                } else if (part[0].equals("Broadcast")) {
+                    broadcast(part);
                 }
             }
             System.out.println("Closed socket for client " + socket.toString());
             in.close();
             out.close();
             socket.close();
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex) {
             Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -165,45 +165,53 @@ public class ServerThread implements Runnable {
         }
     }
 
-    public void login(String[] part) throws IOException, Exception {
-        user = new User();
-        user.setUserName(part[1].trim());
-        user.setPassword(getMD5(part[2].trim()));
-        User us = userDAL.verifyUser(user);
-        name = us.getNickname();
-        String msg = "loginSuccess;" + getStringFromUser(us);
-        if (us.getUserId() != 0) {
-            byte[] encryptedOutput = sc.symmetricEncryption(msg);
-            // Write to client: byte[] encryptedOutput
-            push(encryptedOutput);
-            userDAL.setOnlOff(us.getUserId(), 1);
-            System.out.println("User " + name + "online");
-        } else {
-            byte[] encryptedOutput = sc.symmetricEncryption("Fail");
-            // Write to client: byte[] encryptedOutput
-            push(encryptedOutput);
+    public void login(String[] part) {
+        try {
+            user = new User();
+            user.setUserName(part[1].trim());
+            user.setPassword(getMD5(part[2].trim()));
+            User us = userDAL.verifyUser(user);
+            name = us.getNickname();
+            String msg = "loginSuccess;" + getStringFromUser(us);
+            if (us.getUserId() != 0) {
+                byte[] encryptedOutput = sc.symmetricEncryption(msg);
+                // Write to client: byte[] encryptedOutput
+                push(encryptedOutput);
+                userDAL.setOnlOff(us.getUserId(), 1);
+                System.out.println("User " + name + " online");
+            } else {
+                byte[] encryptedOutput = sc.symmetricEncryption("Fail");
+                // Write to client: byte[] encryptedOutput
+                push(encryptedOutput);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public void register(String[] part) throws SQLException, ParseException, IOException, Exception {
-        user = new User();
-        user.setUserName(part[1]);
-        user.setPassword(getMD5(part[2].trim()));
-        user.setNickname(part[3]);
-        user.setSex(Integer.parseInt(part[4]));
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        Date parsed = format.parse(part[5]);
-        java.sql.Date sql = new java.sql.Date(parsed.getTime());
-        user.setBirthday(sql);
-        if (userDAL.addUser(user) != 0) {
-            String msg = "registerSuccess;";
-            byte[] encryptedOutput = sc.symmetricEncryption(msg);
-            push(encryptedOutput);
-            System.out.println("Add user");
-        } else {
-            byte[] encryptedOutput = sc.symmetricEncryption("Fail");
-            // Write to client: byte[] encryptedOutput
-            push(encryptedOutput);
+    public void register(String[] part) {
+        try {
+            user = new User();
+            user.setUserName(part[1]);
+            user.setPassword(getMD5(part[2].trim()));
+            user.setNickname(part[3]);
+            user.setSex(Integer.parseInt(part[4]));
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            Date parsed = format.parse(part[5]);
+            java.sql.Date sql = new java.sql.Date(parsed.getTime());
+            user.setBirthday(sql);
+            if (userDAL.addUser(user) != 0) {
+                String msg = "registerSuccess;";
+                byte[] encryptedOutput = sc.symmetricEncryption(msg);
+                push(encryptedOutput);
+                System.out.println("Add user");
+            } else {
+                byte[] encryptedOutput = sc.symmetricEncryption("Fail");
+                // Write to client: byte[] encryptedOutput
+                push(encryptedOutput);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
@@ -223,10 +231,6 @@ public class ServerThread implements Runnable {
             }
             byte[] encryptedOutput = sc.symmetricEncryption(msg);
             push(encryptedOutput);
-        } catch (SQLException ex) {
-            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex) {
             Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -241,13 +245,13 @@ public class ServerThread implements Runnable {
                 byte[] encryptedOutput = sc.symmetricEncryption(msg);
                 // Write to client: byte[] encryptedOutput
                 push(encryptedOutput);
-                System.out.println("Create new room successfully, room password is " + part[1]);
+                System.out.println("Create new room successfully, password is " + part[1]);
             } catch (Exception ex) {
                 Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
             }
         } else {
             try {
-                String msg = "createRoomSuccess;" + room.getID() + ";" + part[1];
+                String msg = "createRoomSuccess;" + room.getID();
                 byte[] encryptedOutput = sc.symmetricEncryption(msg);
                 // Write to client: byte[] encryptedOutput
                 push(encryptedOutput);
@@ -255,6 +259,47 @@ public class ServerThread implements Runnable {
             } catch (Exception ex) {
                 Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
             }
+        }
+    }
+
+    public void userStatus(String[] part) {
+        try {
+            List<User> l = userDAL.findUserOnline();
+            String msg = "userStatusSuccess;" + String.valueOf(l.size());
+            for (User us : l) {
+                msg += ";" + us.getNickname() + ";" + String.valueOf(us.getStatus());
+            }
+            byte[] encryptedOutput = sc.symmetricEncryption(msg);
+            push(encryptedOutput);
+        } catch (Exception ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void logout(String[] part) {
+        try {
+            String msg = "logoutSuccess;";
+            byte[] encryptedOutput = sc.symmetricEncryption(msg);
+            push(encryptedOutput);
+            userDAL.setOnlOff(Integer.parseInt(part[1]), 0);
+        } catch (Exception ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void broadcast(String[] part) {
+        try {
+            String msg = "broadcastSuccess;" + part[1];
+            for (ServerThread client : Server.clientList) {
+                    if (!name.equals(client.name)) {
+                        client.out.writeInt(client.sc.symmetricEncryption(msg).length);
+                        client.out.write(client.sc.symmetricEncryption(msg));
+                        client.out.flush();
+                        System.out.println("Server sent '" + part[1] + "' from Client " + name + "--> Client " + client.name);
+                    }
+                }
+        } catch (Exception ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
