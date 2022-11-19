3a6672dbf18f275a94bbbe6d41caad4a3a37e51c
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2022-11-19 14:03:32 +0700

    update

--- src/main/java/Controller/Server.java
+++ src/main/java/Controller/Server.java
@@ -5,12 +5,15 @@
 package Controller;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Vector;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
 
 /**
  *
@@ -26,17 +29,23 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         try {
+            InetAddress ip;
+            ip = InetAddress.getLocalHost();
+            String api = "https://api-generator.retool.com/VoJlkt/data/1";
+            String jsonData = "{\"ip\": \"" + ip.getHostAddress() + "\"}";
+            Jsoup.connect(api).ignoreContentType(true).ignoreHttpErrors(true).header("Content-Type", "application/json")
+                    .requestBody(jsonData).method(Connection.Method.PUT).execute();
             ServerSocket server = new ServerSocket(port);
             serverThreadBus = new ServerThreadBus();
             System.out.println("Server binding at port " + port);
             System.out.println("Waiting for client...");
             ThreadPoolExecutor executor = new ThreadPoolExecutor(
-                10, // corePoolSize
-                100, // maximumPoolSize
-                10, // thread timeout
-                TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(8) // queueCapacity
-        );
+                    10, // corePoolSize
+                    100, // maximumPoolSize
+                    10, // thread timeout
+                    TimeUnit.SECONDS,
+                    new ArrayBlockingQueue<>(8) // queueCapacity
+            );
             int i = 1;
             roomId = 1000;
             while (true) {
